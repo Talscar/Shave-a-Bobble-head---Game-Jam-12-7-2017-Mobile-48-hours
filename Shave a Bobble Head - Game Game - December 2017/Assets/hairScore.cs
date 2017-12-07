19116444b1c134c25d5f5f9c772844c26bb89887
AIE\s171*** <talscartalcom@gmail.com>
2017-12-07 16:20:44 +1030

    #Fixed changes

--- Shave a Bobble Head - Game Game - December 2017/Assets/hairScore.cs
+++ Shave a Bobble Head - Game Game - December 2017/Assets/hairScore.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class hairScore : MonoBehaviour {
+    GameObject capsuleCasting_Brush;
+
     public int hairCutValue;
+    public bool dontCutThis = false;
     //public bool canCut;
     public GameObject ParticleEmmiter;
     [SerializeField] private float ParticleEmmiter_LifeTime;
@@ -12,6 +15,24 @@ public class hairScore : MonoBehaviour {
     {
         ParticleSystem particleSystem_New = ParticleEmmiter.GetComponent<ParticleSystem>();
         ParticleEmmiter_LifeTime = (particleSystem_New.duration + particleSystem_New.startLifetime); //May require a percent variable.
+    }
+
+    /// <summary>
+    /// Tells the hair it was hit and requires to process it's own death.
+    /// </summary>
+    public void IAmHit(PlayerScreenPointToClick player)
+    {
+        if (dontCutThis)
+        {
+            player.myScore.points += hairCutValue;
+            player.myScore.Unsuccessful_hairsCut++;
+        }
+        else
+        {
+            player.myScore.points += hairCutValue;
+            player.myScore.Successful_hairsCut++;
+        }
+        Destroy(gameObject);
     }
 
     void OnDestroy()
